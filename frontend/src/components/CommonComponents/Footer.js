439048425f80a7f06e72e67191e02e6fdbcfdb36
Neeraj Gandhi <64327599+neeraj-22@users.noreply.github.com>
2022-05-03 02:26:07 +0530

    Making Footer a Class Based Component

--- frontend/src/components/CommonComponents/Footer.js
+++ frontend/src/components/CommonComponents/Footer.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import { Fragment } from 'react/cjs/react.production.min';
+import React, { Fragment, Component } from "react";
+import './Footer.css'
 
-import './Footer.css';
-const Footer = () => {
-  return (
-    <Fragment>
-        <footer className='footerContainer bg-primary'>
-            <a href='https://github.com/neeraj-22/tax-bracket' target='_blank' rel="noreferrer noopener" style={{fontWeight:"bold"}}>View in GitHub</a>
+export class Footer extends Component {
+  render() {
+    return (
+      <Fragment>
+        <footer className="footerContainer bg-primary">
+          <a
+            href="https://github.com/neeraj-22/tax-bracket"
+            target="_blank"
+            rel="noreferrer noopener"
+            style={{ fontWeight: "bold" }}
+          >
+            View in GitHub
+          </a>
         </footer>
-        </Fragment>
-  )
+      </Fragment>
+    );
+  }
 }
 
-export default Footer
+export default Footer;
