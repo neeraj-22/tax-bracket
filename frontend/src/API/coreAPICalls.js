f7ad43270c92a090f051219b2da1e68886e94c04
Neeraj Gandhi <64327599+neeraj-22@users.noreply.github.com>
2022-05-03 01:45:18 +0530

    Update coreAPICalls.js

--- frontend/src/API/coreAPICalls.js
+++ frontend/src/API/coreAPICalls.js
@@ -1,18 +1,3 @@
-export const createUser = user => {
-  return fetch(`/api/user`, {
-      method: "POST",
-      headers: {
-          Accept : "application/json",
-          "Content-Type" : "application/json"
-      },
-      body: JSON.stringify(user)
-  })
-  .then(response => {
-      return response.json();
-  })
-  .catch(err => console.log(err))
-}
-
 export const getAllUsers = ()=> {
     return fetch(`/api/users`, { method: "GET" })
     .then(response => {
